5a75bef7a6199a8515fdc661398c2953b34883a5
ann26 <anita@kartoza.com>
2018-07-03 15:04:12 +0700

    update bims functionality

---
+++ django_project/base/static/js/search-utilities.js
@@ -0,0 +1,20 @@
+
+function searchOnMap(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        location.href = '/map/?search=' + $('#search1').val();
+    }
+}
+
+$(document).ready(function () {
+    if(location.pathname === '/map/'){
+        setTimeout(function () {
+            var search = location.search;
+            search = search.replace('?search=', '');
+            $('#search').val(search);
+            $('.search-control').click();
+            var e = $.Event( "keypress", { which: 13 } );
+            $('#search').trigger(e);
+        }, 1200)
+    }
+});
