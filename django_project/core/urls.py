f58bb2fccaf559e953d4e813f814ec743c42ec61
ann26 <anita@kartoza.com>
2018-07-03 16:26:37 +0700

    add upload options view, fish module and reptile module

--- django_project/core/urls.py
+++ django_project/core/urls.py
@@ -23,7 +23,10 @@ urlpatterns = [
     url(r'^accounts/', include('allauth.urls')),
     url(r'^contact/', include('contactus.urls')),
     url(r'^', include('bims.urls')),
+    url(r'^', include('base.urls')),
     url(r'^api-auth/', include('rest_framework.urls')),
+    url(r'^', include(('fish.urls', 'fish'), namespace='fish')),
+    url(r'^', include(('reptile.urls', 'reptile'), namespace='reptile')),
 ]
 
 if settings.DEBUG:
