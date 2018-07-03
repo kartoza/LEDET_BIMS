8852097a2ea319af3fa0052e5523b8ffe6f37219
ann26 <anita@kartoza.com>
2018-07-03 17:01:38 +0700

    search using update url

--- django_project/base/static/js/search-utilities.js
+++ django_project/base/static/js/search-utilities.js
@@ -2,19 +2,6 @@
 function searchOnMap(event) {
     event.preventDefault();
     if (event.keyCode === 13) {
-        location.href = '/map/?search=' + $('#search-on-map').val();
+        location.href = '/map/#search/' + $('#search-on-map').val();
     }
 }
-
-$(document).ready(function () {
-    if(location.pathname === '/map/' && location.search.includes('search')){
-        setTimeout(function () {
-            var search = location.search;
-            search = search.replace('?search=', '');
-            $('#search').val(search);
-            $('.search-control').click();
-            var e = $.Event( "keypress", { which: 13 } );
-            $('#search').trigger(e);
-        }, 1200)
-    }
-});
