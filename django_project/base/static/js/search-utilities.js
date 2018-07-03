059f7a7d7b3bb6e54f302e4458f9c11467766301
ann26 <anita@kartoza.com>
2018-07-03 16:45:05 +0700

    rename element

--- django_project/base/static/js/search-utilities.js
+++ django_project/base/static/js/search-utilities.js
@@ -2,7 +2,7 @@
 function searchOnMap(event) {
     event.preventDefault();
     if (event.keyCode === 13) {
-        location.href = '/map/?search=' + $('#search1').val();
+        location.href = '/map/?search=' + $('#search-on-map').val();
     }
 }
 
