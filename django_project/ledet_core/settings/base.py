ad54396337db029a59bbdc055261e624b039d0a0
Dimas <dimas.ciputra@gmail.com>
2018-07-10 15:34:26 +0700

    Fix staticfiles dir

--- django_project/ledet_core/settings/base.py
+++ django_project/ledet_core/settings/base.py
@@ -59,13 +59,7 @@ STATIC_ROOT = '/home/web/static'
 STATIC_URL = '/static/'
 
 # Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    absolute_path('ledet_core', 'base_static'),
-    absolute_path('ledet_base', 'static'),
-)
+STATICFILES_DIRS = []
 
 # List of finder classes that know how to find static files in
 # various locations.
